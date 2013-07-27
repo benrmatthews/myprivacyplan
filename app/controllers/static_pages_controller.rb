41d02fdfa9d1a971b028aa85ab50b7211a979cc4
benrmatthews <ben@brightone.org.uk>
2013-07-27 14:52:01 +0000

    Added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def search
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def security
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Myprivacyplan::Application.routes.draw do
   
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  get "static_pages/search"
+
+  get "static_pages/terms"
+
+  get "static_pages/privacy"
+
+  get "static_pages/security"
+
   get 'tags/:tag', to: 'ideas#index', as: :tag
   
   resources :comments
